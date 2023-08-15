d263aa00258154680b17269137cbec5bcc6ad2b8
J0hnFe <johnfe@outlook.es>
2023-08-14 19:50:34 -0500

    Taller 38

---
+++ src/main/java/com/example/demo/Pa2U4P4JrAqApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U4P4JrAqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U4P4JrAqApplication.class, args);
+	}
+
+}
